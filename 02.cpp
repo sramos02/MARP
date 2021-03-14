9466733fa3838d35d5c5c27b77983964d85856a7
Sergio <31887668+sramos02@users.noreply.github.com>
2021-03-14 19:19:11 +0100

    Update 02.cpp

--- 02.cpp
+++ 02.cpp
@@ -2,95 +2,96 @@
 
 #include <iostream>
 #include <fstream>
-#include <algorithm>
-#include <climits>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-//La solución priopuesta sigue el esquema de programación dinámica propuesto para el problema de las monedas.
-//Se ha creado un tipo solución que almacenará la información relevante sobre las casillas de la tabla. Cada
-//vez que una solución es mejor que una anterior se insertará en la tabla (siempre lleva el mínimo número de
-//dardos posibles en la mejor solución). El coste del algoritmo es O(score*sections) en el caso peor tanto en
-//espacio como en tiempo. Este algoritmo se podría mejorar en espacio utilizando solamente O(score) utilizando un
-//solo vector que se iria actualizando de derecha a izquierda.
+typedef struct{
+    int prof;
+    int valor;
+}Cofre;
 
-//Para que el resultado se actualice de manera adecuada es importante meter los elementos al principio del
-//vector solución. De esta manera se insertan ordenadamente.
-
-
-typedef struct {
+typedef struct{
     int numSoluciones;
-    int numDardos;
-    vector<int> resultado;
+    int maxOro;
+    int numObjetos;
+    vector<Cofre> cofres;
 }Solucion;
 
-Solucion resolver(const int score, const int sections, const int puntos[]) {
-    Solucion tabla[sections+1][score+1];
 
-    //Init
-     for(int i = 1; i <= score; i++){
-         tabla[0][i].numSoluciones = 0;
-         tabla[0][i].numDardos = INT_MAX;
-     }
+Solucion resolver(int t, int n, const Cofre valores[]) {
+    Solucion tabla[n + 1][t + 1];
+    Solucion ret;
 
-    for(int i = 0; i <= sections; i++){
-        tabla[i][0].numSoluciones = 1;
-        tabla[i][0].numDardos = 0;
+    //Caso base
+    for (int j = 0; j <= t; j++) {
+        tabla[0][j].numSoluciones = 0;
+        tabla[0][j].maxOro = 0;
+        tabla[0][j].numObjetos = 0;
     }
 
-    //Caso recursivo
-    for(int i = 1; i <= sections; i++) {
-        for(int j = 1; j <= score; j++) {
+    for (int i = 0; i <= n; i++) {
+        tabla[i][0].numSoluciones = 1;
+        tabla[i][0].maxOro = 0;
+        tabla[i][0].numObjetos = 0;
+    }
 
-            //Si el tiro actual se pasa de remainingScore no lo cogemos
-            if (puntos[i - 1] > j) tabla[i][j] = tabla[i - 1][j];
+
+    for (int i = 1; i <= n; i++) {
+        int prof = (3 * valores[i - 1].prof); //Tiempo que tardará en bajar
+        for (int j = 1; j <= t; j++) {
+
+            if (prof > j) //Si tardo demasiado en ir mejor no lo cojo
+                tabla[i][j] = tabla[i - 1][j];
             else {
                 int aux1 = tabla[i - 1][j].numSoluciones;
-                int aux2 = tabla[i][j - puntos[i - 1]].numSoluciones;
-                tabla[i][j].numSoluciones =  aux1 + aux2;
+                int aux2 = tabla[i - 1][j - prof].numSoluciones;
+                tabla[i][j].numSoluciones = aux1 + aux2;
 
-                if(aux1 != 0 && aux2 != 0) {
-                    int minimo = min(tabla[i - 1][j].numDardos, tabla[i][j - puntos[i - 1]].numDardos + 1);
-                    tabla[i][j].numDardos = minimo;
-                    if (minimo == tabla[i][j - puntos[i - 1]].numDardos + 1) {
-                        tabla[i][j].resultado = tabla[i][j - puntos[i - 1]].resultado;
-                        tabla[i][j].resultado.insert(tabla[i][j].resultado.begin(), puntos[i - 1]);
+                //Por defecto no cojo el camino
+                tabla[i][j].maxOro = tabla[i - 1][j].maxOro;
+                tabla[i][j].numObjetos = tabla[i - 1][j].numObjetos;
+                tabla[i][j].cofres = tabla[i - 1][j].cofres;
+
+
+                if (aux1 != 0 && aux2 != 0) { //Si tenemos las dos posibilidaddes buscamos la mejor
+                    if (tabla[i - 1][j - prof].maxOro > tabla[i - 1][j].maxOro) {
+                        tabla[i][j].numObjetos = tabla[i - 1][j - prof].numObjetos + 1;
+                        tabla[i][j].maxOro = tabla[i - 1][j - prof].maxOro + valores[i - 1].valor;
+                        tabla[i][j].cofres = tabla[i - 1][j - prof].cofres;
+                        tabla[i][j].cofres.push_back(valores[i - 1]);
                     }
-                    else tabla[i][j].resultado = tabla[i-1][j].resultado;
+                } else if (aux2 != 0) {
+                    tabla[i][j].numObjetos = tabla[i - 1][j - prof].numObjetos + 1;
+                    tabla[i][j].maxOro = tabla[i - 1][j - prof].maxOro + valores[i - 1].valor;
+                    tabla[i][j].cofres = tabla[i - 1][j - prof].cofres;
+                    tabla[i][j].cofres.push_back(valores[i - 1]);
                 }
-                else if (aux2 != 0) {
-                    tabla[i][j].numDardos = tabla[i][j - puntos[i - 1]].numDardos + 1;
-                    tabla[i][j].resultado = tabla[i][j - puntos[i - 1]].resultado;
-                    tabla[i][j].resultado.insert(tabla[i][j].resultado.begin(), puntos[i - 1]);
-                }
-                else if (aux1 != 0){
-                    tabla[i][j].numDardos = tabla[i - 1][j].numDardos;
-                    tabla[i][j].resultado = tabla[i-1][j].resultado;
-                }
+                ret = tabla[i][j];
             }
         }
     }
-    return tabla[sections][score];
+    return ret;
 }
 
 bool resuelveCaso() {
-    int score; //[1-500]
-    int sections; //[1-50]
-    int shots[50];
+    int t, n;
+    Cofre valores[100];
 
-    cin >> score >> sections;
+    cin >> t >> n;
     if (!cin) return false;
-
-    for(int i = 0; i < sections; i++) cin >> shots[i];
-
-    Solucion sol = resolver(score, sections, shots);
-    if(sol.numSoluciones) {
-        cout << sol.numDardos << ": ";
-        for(int i : sol.resultado) cout << i << " ";
-        cout << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> valores[i].prof;
+        cin >> valores[i].valor;
     }
-    else cout << "Imposible\n";
 
+    Solucion ret = resolver(t, n, valores);
+
+    cout << ret.maxOro << endl << ret.numObjetos << endl;
+    if (ret.cofres.size() > 0)
+        for (int i = 0; i < ret.cofres.size(); i++)
+            cout << ret.cofres[i].prof << " " << ret.cofres[i].valor << "\n";
+    cout << "---" << endl;
     return true;
 }
 
