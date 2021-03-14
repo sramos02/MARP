70c9eba226e3fa91661a13d31bcb585142dfed38
Sergio <31887668+sramos02@users.noreply.github.com>
2021-03-14 20:58:36 +0100

    Update 05.cpp

--- 05.cpp
+++ 05.cpp
@@ -18,52 +18,60 @@ typedef struct{
     vector<Cofre> cofres;
 }Solucion;
 
-Solucion resolver(const int t, const int n, const Cofre valores[]) {
+
+Solucion resolver(int t, int n, const Cofre valores[]) {
     Solucion tabla[n + 1][t + 1];
+    Solucion ret;
 
     //Caso base
-    for(int i = 1; i <= n; i++) {
-        tabla[0][i].numSoluciones = 1;
+    for (int j = 0; j <= t; j++) {
+        tabla[0][j].numSoluciones = 0;
+        tabla[0][j].maxOro = 0;
+        tabla[0][j].numObjetos = 0;
+    }
+
+    for (int i = 0; i <= n; i++) {
+        tabla[i][0].numSoluciones = 1;
         tabla[i][0].maxOro = 0;
         tabla[i][0].numObjetos = 0;
     }
 
-    for(int i = 0; i <= t; i++){
-        tabla[0][i].numSoluciones = 0;
-        tabla[0][i].maxOro = 0;
-        tabla[0][i].numObjetos = 0;
-    }
 
-    for(int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++) {
+        int prof = (3 * valores[i - 1].prof); //Tiempo que tardará en bajar
         for (int j = 1; j <= t; j++) {
-            if (3 * valores[i - 1].prof > j) //Si tardo demasiado en ir mejor no lo cojo
+
+            if (prof > j) //Si tardo demasiado en ir mejor no lo cojo
                 tabla[i][j] = tabla[i - 1][j];
             else {
-                int aux1 = tabla[i - 1][j].numSoluciones, aux2 = tabla[i - 1][j - (3 * valores[i - 1].prof)].numSoluciones;
+                int aux1 = tabla[i - 1][j].numSoluciones;
+                int aux2 = tabla[i - 1][j - prof].numSoluciones;
                 tabla[i][j].numSoluciones = aux1 + aux2;
 
-                if (tabla[i - 1][j - 3 * valores[i - 1].prof].maxOro > tabla[i - 1][j].maxOro) { //Decide coger el camino
-                    if (aux1 == 0 || aux2 != 0) {
-                        tabla[i][j].numObjetos = tabla[i - 1][j - (3 * valores[i - 1].prof)].numObjetos + 1;
-                        tabla[i][j].maxOro = tabla[i][j - (3 * valores[i].prof)].maxOro + valores[i].valor;
-                        tabla[i][j].cofres = tabla[i - 1][j - (3 * valores[i - 1].prof)].cofres;
+                //Por defecto no cojo el camino
+                tabla[i][j].maxOro = tabla[i - 1][j].maxOro;
+                tabla[i][j].numObjetos = tabla[i - 1][j].numObjetos;
+                tabla[i][j].cofres = tabla[i - 1][j].cofres;
+
+
+                if (aux1 != 0 && aux2 != 0) { //Si tenemos las dos posibilidaddes buscamos la mejor
+                    if (tabla[i - 1][j - prof].maxOro > tabla[i - 1][j].maxOro) {
+                        tabla[i][j].numObjetos = tabla[i - 1][j - prof].numObjetos + 1;
+                        tabla[i][j].maxOro = tabla[i - 1][j - prof].maxOro + valores[i - 1].valor;
+                        tabla[i][j].cofres = tabla[i - 1][j - prof].cofres;
                         tabla[i][j].cofres.push_back(valores[i - 1]);
-                    } else { //Decide no cogerlo
-                        tabla[i][j].maxOro = tabla[i - 1][j].maxOro;
-                        tabla[i][j].numObjetos = tabla[i - 1][j].numObjetos;
-                        tabla[i][j].cofres = tabla[i - 1][j].cofres;
                     }
+                } else if (aux2 != 0) {
+                    tabla[i][j].numObjetos = tabla[i - 1][j - prof].numObjetos + 1;
+                    tabla[i][j].maxOro = tabla[i - 1][j - prof].maxOro + valores[i - 1].valor;
+                    tabla[i][j].cofres = tabla[i - 1][j - prof].cofres;
+                    tabla[i][j].cofres.push_back(valores[i - 1]);
                 }
-                else {
-                    if (aux1 != 0) {
-                        tabla[i][j].maxOro = tabla[i - 1][j].maxOro;
-                        tabla[i][j].numObjetos = tabla[i - 1][j].numObjetos;
-                        tabla[i][j].cofres = tabla[i - 1][j].cofres;
-                    }
-                }
+                ret = tabla[i][j];
+            }
         }
     }
-    return tabla[n][t];
+    return ret;
 }
 
 bool resuelveCaso() {
@@ -80,9 +88,9 @@ bool resuelveCaso() {
     Solucion ret = resolver(t, n, valores);
 
     cout << ret.maxOro << endl << ret.numObjetos << endl;
-    if (ret.cofres.size() > 0) {
-        for (int i = 0; i < ret.cofres.size(); i++) cout << ret.cofres[i].prof << " " << ret.cofres[i].valor << "\n";
-    }
+    if (ret.cofres.size() > 0)
+        for (int i = 0; i < ret.cofres.size(); i++)
+            cout << ret.cofres[i].prof << " " << ret.cofres[i].valor << "\n";
     cout << "---" << endl;
     return true;
 }
