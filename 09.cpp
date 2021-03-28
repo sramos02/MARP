dcfd644ef39b0d8e40b586908352efd2bbe0daf7
Sergio <31887668+sramos02@users.noreply.github.com>
2021-03-28 20:04:16 +0200

    Update 09.cpp

--- 09.cpp
+++ 09.cpp
@@ -62,3 +62,75 @@ int main() {
 
     return 0;
 }
+
+
+
+/*
+//Sergio Ramos Mesa
+
+#include <iostream>
+#include <fstream>
+
+#include "EnterosInf.h"
+#include "Matriz.h"
+using namespace std;
+
+EntInf buscaCortes(vector<int> const& cortes, int len){
+    int n = cortes.size() - 1;
+
+    Matriz<EntInf> matrices(n+1,n+1,0);
+
+    for (int i = n; i >= 0; i--){
+        for (int j = i+1; j < n; j++) { //si i = j diagonal principal vale 0
+            matrices[i][j] = Infinito;
+
+            //Busca la mínima k de entre todas las posibles
+            for (int k = i; k < n; k++) {
+                EntInf aux1 = matrices[i][k], aux2 = matrices[k+1][j];
+                EntInf esfuerzo = aux1 + aux2;
+
+                if(aux1 == aux2) esfuerzo = esfuerzo + 2*len;
+                else esfuerzo =  esfuerzo + 2*cortes[k]; //len=10  -> corte en 6 -> len1=6,len2=4
+
+                if (esfuerzo < matrices[i][j])
+                    matrices[i][j] = esfuerzo;
+            }
+        }
+    }
+    return matrices[1][n];
+
+
+}
+
+bool resuelveCaso() {
+    int l, n;
+    vector<int> cortes;
+
+    cin >> l >> n;
+    if (l == 0 && n == 0) return false;
+
+    for (int i = 0; i < n; i++) {
+        int aux; cin >> aux;
+        cortes.push_back(aux);
+    }
+
+    cout << buscaCortes(cortes, l) << endl;
+    return true;
+}
+
+int main() {
+#ifndef DOMJUDGE
+    ifstream in("casos.txt");
+    auto cinbuf = cin.rdbuf(in.rdbuf());
+#endif
+
+    while (resuelveCaso());
+
+#ifndef DOMJUDGE
+    cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
+*/
