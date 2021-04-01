7553e4dec90379f6d4ef1c62ed7648114ec04bd4
Sergio <31887668+sramos02@users.noreply.github.com>
2021-04-01 20:00:20 +0200

    Create 15.cpp

---
+++ 15.cpp
@@ -0,0 +1,128 @@
+//Sergio Ramos Mesa
+
+#include <iostream>
+#include <fstream>
+
+#include <queue>
+#include <vector>
+#include "Matriz.h"
+using namespace std;
+
+struct tNodo {
+    int calidad;
+    int prioridad; //calidad estimada
+    int profundidad; //carro actual
+    vector<bool> marcas;
+
+    bool operator<(tNodo const &otro) const { //Comparador de prioridades
+        return otro.prioridad < prioridad;
+    }
+};
+
+void calculaEstimado(int n, int m, Matriz<int> const& calidades, vector<int> & estim){
+
+    for(int i = n - 1; i >= 0; i--) {
+        int max = 0;
+        for(int j = 0; j < m; j++) {
+            if(calidades[i][j] > max)
+                max = calidades[i][j];
+        }
+
+        if(i != n-1) estim[i] = max + estim[i+1];
+        else estim[i] = max;
+    }
+}
+
+int lasQuitanieves(vector<int> const& carros, vector<int> const& caminos, Matriz<int> const& calidades) {
+    int n = carros.size(); //carros
+    int m = caminos.size(); //caminos
+    int mejorCalidad = -1;
+
+    vector<int> estim(n);
+    calculaEstimado(n, m, calidades, estim);
+
+    //Inicializamos la cola de prioridad
+    priority_queue<tNodo> cola = priority_queue<tNodo>();
+    tNodo act;
+    act.marcas = vector<bool>(m, false);
+    act.calidad = 0;
+    act.prioridad = 0;
+    act.profundidad = -1;
+    cola.push(act);
+
+    while (!cola.empty() && cola.top().calidad > mejorCalidad) { //Para todos los carros
+        act = cola.top();
+        cola.pop();
+
+        for (int j = 0; j < m; j++) { //recorre los caminos
+            tNodo hijo(act);
+            hijo.profundidad += 1;
+            int i = hijo.profundidad;
+
+            //Posible solución
+            if (!act.marcas[j] && (carros[i] <= caminos[j])) {
+                hijo.marcas[j] = true;
+                hijo.calidad = hijo.calidad + calidades[i][j];
+
+                if (i < n) hijo.prioridad = hijo.calidad + estim[i + 1];
+                else hijo.prioridad = hijo.calidad;
+
+                //Mejor solución (valor debe ser máximo y llgar al final)
+                if (hijo.prioridad > mejorCalidad) {
+                    if (hijo.profundidad == n - 1)
+                        mejorCalidad = hijo.calidad;
+                    else cola.push(hijo);
+                }
+            }
+        }
+    }
+
+    if(mejorCalidad >= 0) return mejorCalidad;
+    else return 0; //Para el -1 (no encuentra ningun camino posible para ningun quitanieves)
+}
+
+bool resuelveCaso() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> carros(n);
+    vector<int> caminos(m);
+    Matriz<int> calidades(n, m, 0);
+
+    for(int i = 0; i < n; i++) {
+        cin >> carros[i];
+    }
+
+    for (int i = 0; i < m; i++) {
+        cin >> caminos[i];
+    }
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            cin >> calidades[i][j];
+        }
+    }
+
+    int ret = lasQuitanieves(carros, caminos, calidades);
+    cout << ret << endl;
+    return true;
+}
+
+int main() {
+#ifndef DOMJUDGE
+    ifstream in("casos.txt");
+    auto cinbuf = cin.rdbuf(in.rdbuf());
+#endif
+
+    int casos;
+    cin >> casos;
+    for(int i = 0; i < casos; i++)
+        resuelveCaso();
+
+#ifndef DOMJUDGE
+    cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
